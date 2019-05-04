c6c8c290b63bfaa0d62ef11f616c61016168d2d1
Udana Rathnayaka <rathnayakar07@gmail.com>
2019-05-04 18:42:14 +0530

    initial commit

---
+++ LabExercise04/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace LabExercise04
+{
+    
+    public partial class Form1 : Form
+    {
+        LogicOperations lo = new LogicOperations();
+        OpenFileDialog ofd = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select an Image";
+            ofd.Filter = "Images (*.BMP; *.JPG; *.GIF) | *.BMP; *.JPG; *.GIF|" + "All Files(*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lo.LoadOriginalImage(ofd.FileName);
+                String path = ofd.FileName.ToString();
+                pictureBox1.ImageLocation = path;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Select an Image";
+            ofd.Filter = "Images (*.BMP; *.JPG; *.GIF) | *.BMP; *.JPG; *.GIF|" + "All Files(*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lo.LoadMaskImage(ofd.FileName);
+                String path = ofd.FileName.ToString();
+                pictureBox2.ImageLocation = path;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index =comboBox1.SelectedIndex;
+
+            if (index == 0)
+            {
+                lo.ANDOperation();
+                pictureBox3.ImageLocation = "3.jpg";
+            }
+            else if (index == 1)
+            {
+                lo.OROperation();
+                pictureBox3.ImageLocation = "4.jpg";
+            }
+            else if (index == 2)
+            {
+                lo.Subtraction();
+                pictureBox3.ImageLocation = "5.jpg";
+            }
+            else
+            {
+                MessageBox.Show("ERROR !");
+            }
+
+
+        }
+    }
+}
---
+++ LabExercise04/LogicOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace LabExercise04
+{
+    class LogicOperations
+    {
+        IplImage src, mask,andImage,orImage, subtractedImage;
+        public void LoadOriginalImage(String fname)
+        {
+            src = Cv.LoadImage(fname,LoadMode.Color);
+            Cv.SaveImage("1.jpg", src);
+        }
+
+        public void LoadMaskImage(String fname)
+        {
+            mask = Cv.LoadImage(fname, LoadMode.Color);
+            Cv.SaveImage("2.jpg", mask);
+        }
+
+        public void ANDOperation()
+        {
+            andImage = Cv.CreateImage(src.Size, BitDepth.U8, 3);
+            Cv.And(src,mask,andImage);
+            Cv.SaveImage("3.jpg", andImage);
+        }
+
+        public void OROperation()
+        {
+            orImage = Cv.CreateImage(src.Size, BitDepth.U8, 3);
+            Cv.Or(src, mask, orImage);
+            Cv.SaveImage("4.jpg", orImage);
+        }
+
+        public void Subtraction()
+        {
+            subtractedImage = Cv.CreateImage(src.Size, BitDepth.U8, 3);
+            Cv.AbsDiff(src, mask, subtractedImage);
+            Cv.SaveImage("5.jpg", subtractedImage);
+        }
+    }
+}
